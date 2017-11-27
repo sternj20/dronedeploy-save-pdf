f831a3bcf1e076a4163d9206b1693641fba644f8
sternj20 <jessesternmusic@gmail.com>
2017-11-26 23:31:39 -0800

    cleaned up code of script.js file

--- script.js
+++ script.js
@@ -50,51 +50,48 @@ new DroneDeploy({version: 1}).then(function(dronedeploy){
 
 //Draws an item on the canvas
 function drawCanvas(data, x, y){
-  let newCanvas = document.getElementById("newCanvas")
-  let ctx = newCanvas.getContext("2d") 
-  let img = new Image()
-  img.src=data
+  let newCanvas = document.getElementById("newCanvas");
+  let ctx = newCanvas.getContext("2d");
+  let img = new Image();
+  //Base 64 data from call to proxy server
+  img.src=data;
   img.onload = function(){
-
-    ctx.drawImage(img,x,y, 320,320)
-    if (y >= 300) reallyGeneratePDF();
+    ctx.drawImage(img, x, y, 320, 320);
+    //once last tile has been drawn, call generate PDF function
+    if (y >= 300) savePDF();
   }
 }
 
-//Actually generates the PDF
-function reallyGeneratePDF(){
-
+//Saves the PDF
+function savePDF(){
+  document.getElementById('generatePDF').innerText = 'Saving PDF...'
   html2canvas(document.getElementById("newCanvas"), {
-    logging:true,
-    onclone: function(doc){
-      hiddenDiv = doc.getElementById('newCanvas');
-      hiddenDiv.style.display = 'block';
-    },
     onrendered: function(canvas) {
       var imgData = canvas.toDataURL(
-        'image/png');              
+        'image/png');
       var doc = new jsPDF('p', 'mm');
       doc.addImage(imgData, 'png', 10, 10);
-      console.log(imgData)
       doc.save(`${mapName}.pdf`);
-      document.getElementById('generatePDF').innerText = 'Generate PDF'
+      //Change text of button back after PDF has been saved
+      document.getElementById('generatePDF').innerText = 'Generate PDF';
     }
   });
 }
+
 //Loops through tile array, gets base 64 string of tile from proxy server, and draws to canvas
 function generatePDF() {
+  //Change text of button to show user PDF is being generated
   document.getElementById('generatePDF').innerText = 'Generating PDF...'
   let x = 0;
-  let y = 0
-  console.log('drawing tiles onto canvas')
+  let y = 0;
   // Loop through array of tiles returned from API request, for each tile send AJAX get request to my proxy server
   tiles.forEach(function(element, index){
     $.get( `https://pure-gorge-83413.herokuapp.com/?url=${element}`, function( data ) {
       drawCanvas(data, x, y)
-      y += 300
-    })
-  })
-}
+      y += 300;
+    });
+  });
+};
 
 
 
